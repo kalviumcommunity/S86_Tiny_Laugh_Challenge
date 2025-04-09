e8e7d5668228ccc9ac98a3777d713c51db05dee8
siva kumar kalagandha <bhanusree414@gmail.com>
2025-04-09 16:55:54 +0530

    added files

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
 
 // Define a simple /ping route
 app.get("/ping", (req, res) => {
